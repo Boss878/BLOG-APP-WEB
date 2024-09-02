2ea80df0271f8d2152eff7b82a3a205ea1201341
Benaris Arnaut <benaris.arnaut00@icloud.com>
2024-09-02 15:16:01 +0200

    first commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../services/auth/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  username: string = '';
+  password: string = '';
+  errorMessage: string = ''; // Dodano
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  login(): void {
+    this.authService.login(this.username, this.password).subscribe(
+      response => {
+        this.router.navigate(['/home']);
+      },
+      (error: any) => {
+        this.errorMessage = error.error;
+      }
+    );
+  }
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
+}
---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    apiUrl: 'https://localhost:7156/api'  // Ažurirano na novi URL za staging
+  };
+  
